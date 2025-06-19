f0fa9a7b994a6cbeb996899a0645033e76cd322f
Juli124911 <juli12491@gmail.com>
2025-06-19 10:42:48 +0200

    ok

---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+// /** @type {import('vite').Plugin} */
+// const viteServerConfig = {
+// 	name: 'log-request-middleware',
+// 	configureServer(server) {
+// 		server.middlewares.use((req, res, next) => {
+// 			res.setHeader('Access-Control-Allow-Origin', '*');
+// 			res.setHeader('Access-Control-Allow-Methods', 'GET');
+// 			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+// 			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+// 			next();
+// 		});
+// 	}
+// };
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'node_modules/onnxruntime-web/dist/*.jsep.*',
+					dest: 'wasm'
+				}
+			]
+		})
+	],
+	define: {
+		APP_VERSION: JSON.stringify(process.env.npm_package_version),
+		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
+	},
+	build: {
+		sourcemap: true
+	},
+	worker: {
+		format: 'es'
+	},
+server: {
+  host: true,
+  port: 5173,
+  allowedHosts: [
+    'zimablade.taild5d161.ts.net',
+    'localhost',
+    '127.0.0.1'
+  ],
+  proxy: {
+    '/api/': {
+      target: 'http://127.0.0.1:8080',
+      changeOrigin: true,
+      secure: false,
+      ws: true // falls Websockets genutzt werden
+    }
+  }
+}
+
+});
